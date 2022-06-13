9dff52bbcaad054ee3a50811a6f9be0f77896c61
IrinaStarkova <starkovairina12@gmail.com>
2022-06-13 23:42:57 +0300

    добавление модели Users

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+ROLES = (
+    ('user', 'Пользователь'),
+    ('admin', 'Администратор'),
+    ('moderator', 'Модератор')
+)
+
+
+class User(AbstractUser):
+    email = models.EmailField('Почта', max_length=254, unique=True)
+    first_name = models.CharField('Имя', max_length=150, blank=True)
+    last_name = models.CharField('Фамилия', max_length=150, blank=True)
+    bio = models.TextField('Биография', blank=True, null=True)
+    role = models.CharField('Роль', choices=ROLES, default='user')
+    confirmation_code = models.CharField(
+        'Код подтвержения', max_length=100,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self) -> str:
+        return self.username
